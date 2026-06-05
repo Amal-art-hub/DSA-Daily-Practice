7fcc7b30e5d2c813f5be9b3fdd77462e1cbcc59b
Amal krishna <uvamal11@gmail.com>
2026-06-05 22:32:09 +0530

    sum of leftleafnode

--- Tree/practicals.js
+++ Tree/practicals.js
@@ -1043,3 +1043,65 @@ class Node{
  }
 
  console.log(depth(root))
+
+
+ //================sum of left leafnode
+
+ class Node{
+    constructor(data){
+        this.data=data;
+        this.left=null;
+        this.right=null;
+        
+    }
+}
+
+
+ let root=new Node(1);
+ root.left=new Node(2);
+ root.right=new Node(3);
+
+ root.left.left=new Node(4);
+ root.left.right=new Node(5);
+ root.right.left=new Node(6);
+ root.right.right=new Node(7);
+ 
+ 
+ 
+ 
+ 
+ 
+ function sunleftleaf(root){
+     
+     
+     if(!root)return null;
+     let sum=0;
+     
+     
+     function dfs(node){
+         if(node===null)return ;
+         
+         
+         if(node.left && node.left.left===null && node.left.right===null){
+             sum +=node.left.data;
+         }
+         
+         
+         dfs(node.left);
+         dfs(node.right);
+     }
+     
+     dfs(root);
+      return sum;
+     
+ }
+ 
+
+     
+ 
+ 
+ 
+ 
+ 
+ 
+ console.log(sunleftleaf(root))
